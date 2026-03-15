12cc4b8c2c15aacbbcaab4eec76bc4e1344c28a6
PiotrKarolKow <piotr.kowalczyk1210@gmail.com>
2026-03-15 08:15:38 +0000

    wymagania

--- tests/base_test.py
+++ tests/base_test.py
@@ -2,19 +2,15 @@ import unittest
 from selenium import webdriver
 from pages.home_page import HomePage
 
-
 class BaseTest(unittest.TestCase):
-
+    """
+    Base Test for each Test Case
+    """
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        self.driver.get("http://localhost:8080/index.php")
+        self.driver.get("http://localhost:8080")
         self.home_page = HomePage(self.driver)
 
-    def tearDwn(self):
+    def tearDown(self):
         self.driver.quit()
-
-
-
-
-
--- tests/registration_test.py
+++ tests/registration_test.py
@@ -2,11 +2,12 @@ from tests.base_test import BaseTest
 from time import sleep
 
 class RegistrationTest(BaseTest):
-    def setUP(self):
+    def setUp(self):
         super().setUp()
         self.authentication_page = self.home_page.click_sign_in()
-        self.authentication_page.enter_create_account_email("test@test.pl")
+        self.authentication_page.enter_create_account_email("jshjhd@gmail.com")
+        self.create_account_page = self.authentication_page.click_create_account()
 
-    def testNoSurname(self):
-        sleep(4)
-        pass
+    def testNoLastname(self):
+        self.create_account_page.enter_first_name("Marcin")
+        sleep(3)
